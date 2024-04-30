a02be6431c1f12c377902647eb5860722208568a
Enoc Falcón <83475399+falcon-enoc@users.noreply.github.com>
2024-04-29 22:02:58 -0400

    correcta extraccion de informacion del archivo
    ahora la información del archivo está parseada en numero_punteros, tamano_cluster. Los vectores están siendo guardados en una matriz

--- sistemas-distribuidos/eval2/tarea.c
+++ sistemas-distribuidos/eval2/tarea.c
@@ -4,41 +4,77 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define T 8
+#define DIM 20
+
+void imprimir_matriz(float **matriz, int numero_puntos);
 
 int main() {
     FILE *archivo;
-    char linea[100];
+    char linea[1024];
     int numero_puntos;
     int tamano_cluster;
 
     archivo = fopen("test.txt", "r");
 
-    if (archivo == NULL) {
+    if (archivo == NULL) { //comprobaciones apertura de archivo
         printf("No se pudo abrir el archivo.\n");
         return 1;
     }
 
-    // Leer las dos primeras líneas del archivo
-    for (int i = 0; i < 2; i++) {
-        if (fgets(linea, sizeof(linea), archivo) != NULL) {
-            if (i == 0) {
-                numero_puntos = atoi(linea);
-                printf("Número de puntos: %d\n", numero_puntos);
-            } else if (i == 1) {
-                tamano_cluster = atoi(linea);
-                printf("Tamaño del cluster: %d\n", tamano_cluster);
+	// Obtener las 2 primeras lineas del archivo
+	fgets(linea, sizeof(linea), archivo);
+    numero_puntos = atoi(linea);
+    fgets(linea, sizeof(linea), archivo);
+    tamano_cluster = atoi(linea);
+
+	// Crear una matriz dinámica para almacenar los puntos
+    float **matriz = malloc(numero_puntos * sizeof(float *));
+    for (int i = 0; i < numero_puntos; i++) {
+        matriz[i] = malloc(DIM * sizeof(float));
+        if (matriz[i] == NULL) {
+            printf("Fallo la asignación de memoria para la fila %d.\n", i);
+            // Liberar memoria previamente asignada
+            for (int j = 0; j < i; j++) {
+                free(matriz[j]);
             }
+            free(matriz);
+            fclose(archivo);
+            return 1;
         }
     }
-
-    // Leer y mostrar el resto del archivo
-    printf("Resto del archivo:\n");
-    while (fgets(linea, sizeof(linea), archivo) != NULL) {
-        printf("%s", linea); // Imprimir o almacenar según tus necesidades
+	    // Leer y almacenar los datos en la matriz
+    int indice = 0;
+    while (fgets(linea, sizeof(linea), archivo) != NULL && indice < numero_puntos) {
+        char *token = strtok(linea, " ");
+        int j = 0;
+        while (token != NULL && j < DIM) {
+            matriz[indice][j] = atof(token); // Cambiado a atof para convertir a float
+            token = strtok(NULL, " ");
+            j++;
+        }
+        indice++;
     }
 
     fclose(archivo);
+
+	imprimir_matriz(matriz, numero_puntos);
+
+    // Liberar la memoria de la matriz
+    for (int i = 0; i < numero_puntos; i++) {
+        free(matriz[i]);
+    }
+    free(matriz);
+
     return 0;
+}
+
+void imprimir_matriz(float **matriz, int numero_puntos) {
+    for (int i = 0; i < numero_puntos; i++) {
+        for (int j = 0; j < DIM; j++) {
+            printf("%.3f ", matriz[i][j]);
+        }
+        printf("\n");
+    }
 }
