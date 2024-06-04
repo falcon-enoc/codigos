98327a389275873c0e94a3019ed230e9c9cb2c6c
Enoc Falcón <83475399+falcon-enoc@users.noreply.github.com>
2024-06-03 23:05:35 -0400

    ejercicios mpi

--- sistemas-distribuidos/mpi2.c
+++ sistemas-distribuidos/mpi2.c
@@ -1,4 +1,3 @@
-//  Basicos de MPI
 #include <stdio.h>
 #include <mpi.h>
 
@@ -8,7 +7,9 @@ int main(int argc, char **argv){
     int yo;
     int numero = 101;
     int recibido;
+    int bandera;
     MPI_Status estado;
+    MPI_Request req;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &nproc); // Se rescata el Número de procesos
@@ -17,10 +18,15 @@ int main(int argc, char **argv){
     printf("Soy el proceso: %d de un total de %d\n", yo, nproc);
 
     if (yo == 0) {
-        MPI_Send(&numero, 1, MPI_INT, 2, 111, MPI_COMM_WORLD);
+        MPI_Isend(&numero, 1, MPI_INT, 2, 111, MPI_COMM_WORLD, &req);
     }
     if (yo == 2) {
-        MPI_Recv(&recibido, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &estado);
+        MPI_Irecv(&recibido, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &req);
+        bandera = 0;
+        while (bandera == 0) {
+            // Aca se puede hacer cosas mientras no llegue el mensaje.
+            MPI_Test(&req, &bandera, &estado);
+        }
         printf("Proceso [%d] :: recibido = %d\n", yo, recibido);
     }
 
